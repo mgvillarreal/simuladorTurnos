a1108e1262b5bda1aeef08f63ba3e65c1e6e9876
Maria Gabriela <mgvillarreal09@gmail.com>
2024-10-21 23:35:40 -0300

    agrego librerias

--- js/simuladorTurnos.js
+++ js/simuladorTurnos.js
@@ -4,10 +4,14 @@ let fechaSeleccionada = null;
 let horaSeleccionada = null;
 let mesActual = new Date().getMonth();
 let anioActual = new Date().getFullYear();
+let turnosDisponibles = [];
+let fechasDisponibles = [];
+let horasDisponibles = [];
 const calendario = document.getElementById('calendario');
 const mesActualDisplay = document.getElementById('mes-actual');
-const fechasDisponibles = ["2024-10-10", "2024-10-12", "2024-10-15", "2024-11-05"];
-const horasDisponibles = [9, 10, 11, 12, 19, 8];
+/*const fechasDisponibles = ["2024-10-10", "2024-10-12", "2024-10-15", "2024-11-05"];
+const horasDisponibles = [9, 10, 11, 12, 19, 8];*/
+const { DateTime } = luxon;
 
 /* -- FUNCIONES -- */
 function seleccionarEspecialidad(especialidad) { //Guarda la especialidad seleccionada en localStorage
@@ -21,9 +25,11 @@ function traerDatosDeLocalStorage(){
         const fecha = localStorage.getItem('fechaTurno');
         const hora = localStorage.getItem('horaTurno');
 
+        const fechaFormateada = convertirFormatoFecha(fecha);
+
         const turnoGuardado = {
             especialidadGuardada: especialidad,
-            fechaGuardada: fecha,
+            fechaGuardada: fechaFormateada,
             horaGuardada: hora,
             nombreGuardado: datosTurno.nombre,
             apellidoGuardado: datosTurno.apellido,
@@ -36,17 +42,11 @@ function traerDatosDeLocalStorage(){
     }
 }
 
-function mostrarConfirmacion(){ //Toma los datos del localStorage para mostrar mensaje
-    const mensaje = document.getElementById('mensaje');
-    mensaje.classList.remove('d-none');
+function convertirFormatoFecha(fecha){
+    let fechaLuxon = DateTime.fromISO(fecha);
+    let fechaFormateada = fechaLuxon.toFormat('dd-MM-yyyy');
 
-    const turnoGuardado = traerDatosDeLocalStorage();
-
-    mensaje.innerHTML = `<p class="mensaje">Se ha confirmado un turno para ${turnoGuardado.nombreGuardado} ${turnoGuardado.apellidoGuardado} en la especialidad de ${turnoGuardado.especialidadGuardada}, el día ${turnoGuardado.fechaGuardada} a las ${turnoGuardado.horaGuardada}:00hs.<p>`;
-
-    mensaje.classList.remove('d-none');
-    
-    document.getElementById('turnoForm').reset();
+    return fechaFormateada;
 }
 
 function mostrarTurnoGuardado(){
@@ -73,18 +73,49 @@ function mostrarBloque(idBloque){
     document.getElementById(idBloque).style.display = 'block';
 }
 
+async function buscarTurnosDisponibles(){
+    try {
+        const turnosJSON = await fetch("./turnosDisponibles.json");
+        turnosDisponibles = await turnosJSON.json();
+        console.log(turnosDisponibles);        
+    } catch (error) {
+        console.warn("Error al buscar los turnos disponibles.");
+    }
+}
+
+function seleccionarTurno(especialidadSeleccionada){
+    console.log("entré a la funcion");
+    console.log("Especialidad: ", especialidadSeleccionada);
+    
+    try{
+        for(let i=0; i<turnosDisponibles.length; i++){
+            if(turnosDisponibles[i].especialidad == especialidadSeleccionada){
+                console.log("coincide");
+
+                fechasDisponibles = turnosDisponibles[i].fechas;
+                horasDisponibles = turnosDisponibles[i].horas;
+            }
+        }
+        console.log("Fechas dispo: ", fechasDisponibles)
+        console.log("Horas dispo: ", horasDisponibles)
+    }
+    catch (error) {
+        console.warn("Error al seleccionar los turnos disponibles.");
+    }
+}
+
 /* -- CAMBIO DINAMICO DE FECHAS Y HORAS -- */
 /* Muestra calendario */
 function cargarCalendario(){
     calendario.innerHTML = `
-      <div class="dia-semana">L</div>
-      <div class="dia-semana">M</div>
-      <div class="dia-semana">M</div>
-      <div class="dia-semana">J</div>
-      <div class="dia-semana">V</div>
-      <div class="dia-semana">S</div>
-      <div class="dia-semana">D</div>
-      `;
+        <div class="dia-semana">L</div>
+        <div class="dia-semana">M</div>
+        <div class="dia-semana">M</div>
+        <div class="dia-semana">J</div>
+        <div class="dia-semana">V</div>
+        <div class="dia-semana">S</div>
+        <div class="dia-semana">D</div>
+        `;
     
     //Muestra el mes y el año actual
     const mesActualNombre = new Date(anioActual, mesActual).toLocaleString('default', {month: 'long', year: 'numeric'});
@@ -95,34 +126,34 @@ function cargarCalendario(){
     
     //Crea espacios vacíos para los días antes del primer día del mes
     for (let i = 0; i < primerDiaMes; i++){
-      const emptyDiv = document.createElement('div');
-      calendario.appendChild(emptyDiv);
+        const emptyDiv = document.createElement('div');
+        calendario.appendChild(emptyDiv);
     }
   
     //Genera los días del mes
     for (let dia = 1; dia <= diasEnElMes; dia++){
-      const fechaDiv = document.createElement('div');
-      const fechaStr = `${anioActual}-${(mesActual + 1).toString().padStart(2, '0')}-${dia.toString().padStart(2, '0')}`;
+        const fechaDiv = document.createElement('div');
+        const fechaStr = `${anioActual}-${(mesActual + 1).toString().padStart(2, '0')}-${dia.toString().padStart(2, '0')}`;
       
-      fechaDiv.textContent = dia;
+        fechaDiv.textContent = dia;
       
-      if (fechasDisponibles.includes(fechaStr)){
-          fechaDiv.classList.add('disponible');
-          fechaDiv.addEventListener('click', function(){
-              document.querySelectorAll('.calendario-grid div').forEach(div => div.classList.remove('seleccion'));
-              this.classList.add('seleccion');
-              fechaSeleccionada = fechaStr;
-          });
-      } else{
-          fechaDiv.classList.add('nodisponible');
-      }
+        if(fechasDisponibles.includes(fechaStr)){
+            fechaDiv.classList.add('disponible');
+            fechaDiv.addEventListener('click', function(){
+                document.querySelectorAll('.calendario-grid div').forEach(div => div.classList.remove('seleccion'));
+                this.classList.add('seleccion');
+                fechaSeleccionada = fechaStr;
+            });
+        } else{
+            fechaDiv.classList.add('nodisponible');
+        }
   
-      calendario.appendChild(fechaDiv);
+        calendario.appendChild(fechaDiv);
     }
 }
   
 /* Navegación entre meses */
- document.getElementById('mes-previo').addEventListener('click', () => {
+document.getElementById('mes-previo').addEventListener('click', () => {
     mesActual--;
     if (mesActual < 0){
         mesActual = 11;
@@ -172,9 +203,14 @@ const cards = document.querySelectorAll('.card');
 cards.forEach((card) => {
     card.addEventListener('click', () => {
         const especialidadSeleccionada = card.innerText;
+        seleccionarTurno(especialidadSeleccionada);
         seleccionarEspecialidad(especialidadSeleccionada);
-        ocultarBloque('containerEspecialidades');
-        mostrarBloque('containerTurnos');
+
+        setTimeout(()=>{
+            ocultarBloque('containerEspecialidades');
+            mostrarBloque('containerTurnos');
+            ocultarBloque('containerEspecialidades');
+        }, 1500);
     });
 });
 
@@ -192,6 +228,7 @@ document.getElementById('boton-guardar').addEventListener('click', () =>{
 document.getElementById('turnoForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
+    //Guarda datos del turno en localStorage
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const email = document.getElementById('email').value;
@@ -205,17 +242,31 @@ document.getElementById('turnoForm').addEventListener('submit', function (event)
     localStorage.setItem('turno', JSON.stringify(turno));
 
     ocultarBloque('containerFormulario');
-    mostrarBloque('containerMensaje');
-    mostrarConfirmacion();
-    
-    document.getElementById('turnoForm').reset();
-});
 
-/* Acepta mensaje de confirmación */
-document.getElementById('boton-aceptar').addEventListener('click', () =>{
-    ocultarBloque('containerMensaje');
+    //Muestra alerta de confirmación de turno
+    const turnoGuardado = traerDatosDeLocalStorage();
+
+    Swal.fire({
+        title: "Confirmar turno",
+        text: `¿Está seguro de solicitar un turno para la especialidad de ${turnoGuardado.especialidadGuardada}, el día ${turnoGuardado.fechaGuardada} a las ${turnoGuardado.horaGuardada}:00hs?`,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#0056b3",
+        cancelButtonColor: "d5d8dc",
+        confirmButtonText: "Confirmar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Turno Confirmado!",
+                text: `El turno para el paciente ${nombre} ${apellido} se ha guardado correctamente.`,
+                icon: "success",
+                confirmButtonColor: "#0056b3"
+            });
+        }
+    });
+
+    document.getElementById('turnoForm').reset();
     mostrarBloque('containerEspecialidades');
-    mostrarTurnoGuardado();
 });
   
 /* CARGA CALENDARIO Y HORARIO */
@@ -223,4 +274,5 @@ window.onload = () =>{
     cargarCalendario();
     cargarHorarios();
     mostrarTurnoGuardado();
+    buscarTurnosDisponibles();
 };
